f870ea692a8208511ac457b2624ce783b25493a0
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2020-10-10 20:23:00 -0400

    Made core much less spammy when influx is not running

--- frontend/crowdeye/influx.py
+++ frontend/crowdeye/influx.py
@@ -1,16 +1,22 @@
 from influxdb import InfluxDBClient
 import datetime
 import pytz
+import requests
 
+# Only prints the error once to avoid spamm
+printedDBError = False
 
 def writeEntry(nodeID, crossed_left, crossed_right, total_crossed, total_people):
     """
-    writeEntry is a function to write data read the from the nodes into an InfluxDB database \n
+    writeEntry is a function to write data read the from the nodes into an InfluxDB database  
     The params are, in order: nodeID, crossed_left, crossed_right, total_crossed, total_people
     """
-    #Get the current time in the RFC3339 format for InfluxDB
+    global printedDBError
+    # Get the current time in the RFC3339 format for InfluxDB
     t = datetime.datetime.utcnow()
-    t = t.isoformat("T") + "Z" #Z is for Zulu timezone, idk why I need it but stackoverflow says to
+    t = t.isoformat("T") + "Z" # Z is for Zulu timezone, idk why I need it but stackoverflow says to
+
+    # Make the json body to send to the database
     json_body = [
         {
             "measurement": "people",
@@ -26,6 +32,15 @@ def writeEntry(nodeID, crossed_left, crossed_right, total_crossed, total_people)
             }
         }
     ]
-    client = InfluxDBClient('localhost', 8086, 'admin', 'admin', 'db')
-    client.write_points(json_body)
-
+    # Try to establish the connection with the database and write the data
+    # If the database is not connected, fail silently
+    try:
+        client = InfluxDBClient('localhost', 8086, 'admin', 'admin', 'db')
+        client.write_points(json_body)
+    
+    # Check to see if there was a connection error
+    except requests.exceptions.ConnectionError:
+        # Check if the error message has been printed, and if not, print it
+        if not printedDBError:
+            print("Not able to open the database")
+            printedDBError = True
--- frontend/crowdeye/management/commands/startserver.py
+++ frontend/crowdeye/management/commands/startserver.py
@@ -12,20 +12,27 @@ from ...tasks import AI_CORE_IP
 
 DATA = {}
 DATA_LIST = []
+printedWriteError = False
 
 def get_cams():
     global DATA_LIST
     global DATA
+    global printedWriteError
+    
     while True:
         try:
+            # Try getting the cameras
             all_cams = []
-            print("-", end="", flush=True)
+            # print("-", end="", flush=True)
+            # Make a call to the api for all of the camera data
             x = requests.get(AI_CORE_IP + "/" + "get_cameras")
+            # Put the data into a variable
             data = x.json()
 
-            print(data)
+            # print(data)
 
             temp = []
+            # Itterate over the data and construct it into an array of dicts
             for node_id in data:
                 x = requests.get(AI_CORE_IP + "/" + "camera" + "/" + node_id)
                 tmp = x.json()
@@ -33,9 +40,15 @@ def get_cams():
                 temp.append(tmp)
             DATA_LIST = temp
 
-
-            print('t', DATA_LIST)
-            writeEntry(DATA_LIST[0]["node_id"], DATA_LIST[0]["crossed_left"], DATA_LIST[0]["crossed_right"], DATA_LIST[0]["total_crossed"], DATA_LIST[0]["total_people"])
+            # print('t', DATA_LIST)
+            
+            # Try to write to the database
+            try:
+                writeEntry(**DATA_LIST[0])
+            except:
+                if not printedWriteError:
+                    print(":(")
+                    printedWriteError = True
 
             time.sleep(2)
         except Exception as e:
